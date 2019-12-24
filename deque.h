68d08f59c6c3571ae0001799901dd375dc3693db
Michał <enedil.isildur@gmail.com>
2019-12-24 04:15:11 +0100

    blocking queue

--- deque.h
+++ deque.h
@@ -1,6 +1,7 @@
 #ifndef DEQUE_H
 #define DEQUE_H
 
+#include <assert.h>
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdlib.h>
@@ -22,6 +23,22 @@ typedef struct deque {
     size_t size;
     node_t begin, end;
 } deque_t;
+
+typedef struct blocking_deque {
+    deque_t deque;
+    pthread_mutex_t lock;
+    pthread_mutexattr_t lock_attr;
+    pthread_cond_t cond;
+} blocking_deque_t;
+
+void deque_init(deque_t *d);
+void deque_destroy(deque_t *d);
+size_t deque_size(deque_t *d);
+int deque_is_empty(deque_t *d); 
+int deque_push_front(deque_t *d, type * val);
+int deque_push_back(deque_t *d, type * val);
+int deque_pop_front(deque_t *d, type * val);
+int deque_pop_back(deque_t *d, type * val);
 
 void deque_init(deque_t *d) {
     d->size = 0;
@@ -105,6 +122,149 @@ int deque_pop_back(deque_t *d, type * val) {
     back->prev->next = &d->end;
 
     free(back);
+
+    return OK;
+}
+
+static int _mutexattr_init(pthread_mutexattr_t *attr) {
+    int err;
+    if ((err = pthread_mutexattr_init(attr))) {
+        return err;
+    }
+    if ((err = pthread_mutexattr_settype(attr, PTHREAD_MUTEX_ERRORCHECK))) {
+        pthread_mutexattr_destroy(attr);
+        return err;
+    }
+    if ((err = pthread_mutexattr_setrobust(attr, PTHREAD_MUTEX_ROBUST))) {
+        pthread_mutexattr_destroy(attr);
+        return err;
+    }
+    return OK;
+}
+
+static int _mutex_init(pthread_mutex_t *mutex, pthread_mutexattr_t *attr) {
+    int err;
+    if ((err = _mutexattr_init(attr))) {
+        return err;
+    }
+    if ((err = pthread_mutex_init(mutex, attr))) {
+        pthread_mutexattr_destroy(attr);
+        return err;
+    }
+    return OK;
+}
+
+static void _mutex_destroy(pthread_mutex_t *mutex, pthread_mutexattr_t *attr) {
+    pthread_mutex_destroy(mutex);
+    pthread_mutexattr_destroy(attr);
+}
+
+int blocking_deque_init(blocking_deque_t *d) {
+    int err;
+    memset(d, 0, sizeof(blocking_deque_t));
+    if ((err = _mutex_init(&d->lock, &d->lock_attr))) {
+        return err;
+    }
+
+    if ((err = pthread_cond_init(&d->cond, NULL))) {
+        _mutex_destroy(&d->lock, &d->lock_attr);
+        return err;
+    }
+
+    deque_init(&d->deque);
+    return OK;
+}
+
+void blocking_deque_destroy(blocking_deque_t *d) {
+    deque_destroy(&d->deque);
+    _mutex_destroy(&d->lock, &d->lock_attr);
+    pthread_cond_destroy(&d->cond);
+}
+
+size_t blocking_deque_size(blocking_deque_t *d) {
+    size_t ret;
+    pthread_mutex_lock(&d->lock);
+    ret = deque_size(&d->deque);
+    pthread_mutex_unlock(&d->lock);
+    return ret;
+}
+
+int blocking_deque_is_empty(blocking_deque_t *d) {
+    return blocking_deque_size(d) == 0;
+}
+
+int blocking_deque_push_front(blocking_deque_t *d, type * val) {
+    int err;
+    if ((err = pthread_mutex_lock(&d->lock)))
+        return err;
+    if ((err = deque_push_front(&d->deque, val)))
+        return err;
+    if (deque_size(&d->deque) == 1 && (err = pthread_cond_signal(&d->cond)))
+        goto POP;
+    if ((err = pthread_mutex_unlock(&d->lock)))
+        goto POP;
+    return OK;
+
+POP:;
+    type t;
+    deque_pop_front(&d->deque, &t);
+    return err;
+}
+
+
+int blocking_deque_push_back(blocking_deque_t *d, type * val) {
+    int err;
+    if ((err = pthread_mutex_lock(&d->lock)))
+        return err;
+    if ((err = deque_push_back(&d->deque, val)))
+        return err;
+    if (deque_size(&d->deque) == 1 && (err = pthread_cond_signal(&d->cond)))
+        goto POP;
+    if ((err = pthread_mutex_unlock(&d->lock)))
+        goto POP;
+    return OK;
+
+POP:;
+    type t;
+    deque_pop_back(&d->deque, &t);
+    return err;
+
+}
+
+int blocking_deque_pop_front(blocking_deque_t *d, type * val) {
+    int err = 0;
+    if ((err = pthread_mutex_lock(&d->lock)))
+        return err;
+    
+    while (deque_is_empty(&d->deque) && err == 0) {
+        err = pthread_cond_wait(&d->cond, &d->lock);
+    }
+    if (err) {
+        pthread_mutex_unlock(&d->lock);
+        return err;
+    }
+    assert(deque_pop_front(&d->deque, val) == 0);   // should not fail in any case
+
+    pthread_mutex_unlock(&d->lock);
+
+    return OK;
+}
+
+int blocking_deque_pop_back(blocking_deque_t *d, type * val) {
+    int err = 0;
+    if ((err = pthread_mutex_lock(&d->lock)))
+        return err;
+    
+    while (deque_is_empty(&d->deque) && err == 0) {
+        err = pthread_cond_wait(&d->cond, &d->lock);
+    }
+    if (err) {
+        pthread_mutex_unlock(&d->lock);
+        return err;
+    }
+    assert(deque_pop_back(&d->deque, val) == 0);   // should not fail in any case
+
+    pthread_mutex_unlock(&d->lock);
 
     return OK;
 }
